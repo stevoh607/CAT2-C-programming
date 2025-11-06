754252708e6081d623fc192b09dfd361733645bb
stevoh607 <stephenndungu230@gmail.com>
2025-11-06 11:55:29 +0300

    Add files via upload

---
+++ CAT2_Q1.c
@@ -0,0 +1,50 @@
+/*
+Name:Stephen Ndungu
+Reg no:CT101/G/26448/25
+
+
+*/
+
+#include<stdio.h> 
+int main(){
+	int i,j,k;
+	int scores [2][2][2]={
+	{
+		{65,92},
+		{84,72}
+	},
+	{
+		
+		{35,70},
+            {59,67}
+	}		
+				
+		
+	};
+	
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			for(k=0;k<2;k++){
+			
+			
+			
+		printf("%d",scores[i][j][k]);
+			
+			
+			
+			
+			}	
+			printf("\n");
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
